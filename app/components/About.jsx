d12d6686d9072f35cf3a49c5e4ad4ed4317c8577
unknown <phamluongtai@gmail.com>
2017-10-19 01:44:43 -0700

    Update copy

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var About = React.createClass({
     render: function () {
         return (
-            <h3>About Component</h3>
+            <h3>About Component !</h3>
         )
     }
 });
