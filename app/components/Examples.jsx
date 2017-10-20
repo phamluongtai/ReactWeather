968e0202e6074a6ed8d77221c0bafe36dd83a81e
unknown <phamluongtai@gmail.com>
2017-10-19 18:38:33 -0700

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,11 @@ var React = require('react');
 var Examples = React.createClass({
     render: function () {
         return (
+        <div>
             <h3>Examples Component</h3>
+            <p>Welcome to examples page!</p>
+        </div>
+
         )
     }
 });
