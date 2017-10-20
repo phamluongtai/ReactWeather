3c7bca7d934e14cdc3a8081ce2d5f744559c7167
unknown <phamluongtai@gmail.com>
2017-10-20 01:40:48 -0700

    Style about page and link to resources

--- public/bundle.js
+++ public/bundle.js
@@ -26720,29 +26720,56 @@
 /* 255 */
 /***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
-	var About = React.createClass({
-	    displayName: 'About',
 
-	    render: function render() {
-	        return React.createElement(
-	            'div',
+	var About = function About(props) {
+	    return React.createElement(
+	        "div",
+	        null,
+	        React.createElement(
+	            "h1",
+	            { className: "text-center" },
+	            "About"
+	        ),
+	        React.createElement(
+	            "p",
+	            null,
+	            "This is a weather application build on React. I have built this for The Complete React Web App Developer Course."
+	        ),
+	        React.createElement(
+	            "p",
+	            null,
+	            "Here are some of the tools I used:"
+	        ),
+	        React.createElement(
+	            "ul",
 	            null,
 	            React.createElement(
-	                'h3',
+	                "li",
 	                null,
-	                'About'
+	                React.createElement(
+	                    "a",
+	                    { href: "https://facebook.github.io/react" },
+	                    "React"
+	                ),
+	                " - This was the JavaScript framework used."
 	            ),
 	            React.createElement(
-	                'p',
+	                "li",
 	                null,
-	                'Welcome to the about page.'
+	                React.createElement(
+	                    "a",
+	                    { href: "http://openweathermap.org" },
+	                    "Open Weather Map"
+	                ),
+	                " - I used Open Weather Map to search for weather data by city name."
 	            )
-	        );
-	    }
-	});
+	        )
+	    );
+	};
+
 	module.exports = About;
 
 /***/ }),
